2ab42fad6c0027b40cfe525f573626c437142fb4
Eliot Carlsen and Rob McKenzie <eliot@>
2017-02-15 10:45:18 -0800

    add form that converts user input to upper case.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,13 @@ $(document).ready(function() {
     var userInput = $("#username").val();
     $(".nameOne").text(userInput);
 
-
-
-
-
-
-
-
-
-
     event.preventDefault();
 
+  });
+  $(".upperCase").submit(function(event) {
+    var sentenceInput = $("#toUpperCase").val().toUpperCase();
+    $(".upperResponse").text(sentenceInput);
+
+    event.preventDefault();
   });
 });
